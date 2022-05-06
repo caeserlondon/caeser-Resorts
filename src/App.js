7b01e3bd237c4c98d4af531ad9c37ec12b1528b9
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-05-06 22:44:19 +0700

    Adding the Router to the App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React from 'react';
+
+import { Routes, Route } from 'react-router-dom';
+
 import './App.css';
 
-import Home from './pages/Home';
+import Home from './pages/Home.js';
 import Rooms from './pages/Rooms';
 import SingleRoom from './pages/SingleRoom';
 import ErrorPage from './pages/ErrorPage';
 
 function App() {
 	return (
-		<>
-			<Home />
-			<Rooms />
-			<SingleRoom />
-			<ErrorPage />
-		</>
+		<Routes>
+			<Route path="/" element={<Home />}></Route>
+			<Route path="/rooms/" element={<Rooms />}></Route>
+			<Route path="/rooms/:slug" element={<SingleRoom />}></Route>
+			<Route path="*" element={<ErrorPage />} />
+		</Routes>
 	);
 }
 
