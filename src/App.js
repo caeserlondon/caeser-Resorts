44710288eb8b7dee77d0f33e27ecde7a2d0dbb79
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-05-07 01:19:42 +0700

    Adding the Navbar component to the App.js

--- src/App.js
+++ src/App.js
@@ -9,14 +9,19 @@ import Rooms from './pages/Rooms';
 import SingleRoom from './pages/SingleRoom';
 import ErrorPage from './pages/ErrorPage';
 
+import Navbar from './component/Navbar';
+
 function App() {
 	return (
-		<Routes>
-			<Route path="/" element={<Home />}></Route>
-			<Route path="/rooms/" element={<Rooms />}></Route>
-			<Route path="/rooms/:slug" element={<SingleRoom />}></Route>
-			<Route path="*" element={<ErrorPage />} />
-		</Routes>
+		<>
+			<Navbar />
+			<Routes>
+				<Route path="/" element={<Home />}></Route>
+				<Route path="/rooms/" element={<Rooms />}></Route>
+				<Route path="/rooms/:id" element={<SingleRoom />}></Route>
+				<Route path="*" element={<ErrorPage />} />
+			</Routes>
+		</>
 	);
 }
 
