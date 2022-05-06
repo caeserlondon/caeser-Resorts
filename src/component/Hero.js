f422e97b959dd62584dd45cfec3fe884f32cf5f3
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-05-07 03:22:10 +0700

    Adding the props to the Hero Component

--- src/component/Hero.js
+++ src/component/Hero.js
@@ -3,3 +3,7 @@ import React from 'react';
 export default function Hero({ hero, children }) {
 	return <header className={hero}>{children}</header>;
 }
+
+Hero.defaultProps = {
+	hero: 'defaultHero',
+};
