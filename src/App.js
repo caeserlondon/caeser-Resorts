aff28775c22314285ef2145ca06569a285d8f181
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-05-06 19:26:15 +0700

    Adding the pages to the App.j

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-// import logo from './logo.svg';
-// import './App.css';
+import React from 'react';
+import './App.css';
+
+import Home from './pages/Home';
+import Rooms from './pages/Rooms';
+import SingleRoom from './pages/SingleRoom';
+import ErrorPage from './pages/ErrorPage';
 
 function App() {
-	return <>Dubai Resorts</>;
+	return (
+		<>
+			<Home />
+			<Rooms />
+			<SingleRoom />
+			<ErrorPage />
+		</>
+	);
 }
 
 export default App;
