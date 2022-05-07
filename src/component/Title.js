981104d4a434bed5698c1e58b4404be079dae592
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-05-07 21:38:52 +0700

    Fixing the Title.js

--- src/component/Title.js
+++ src/component/Title.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Title({ title }) {
 	return (
 		<div className="section-title">
-			<h4>Services</h4>
+			<h4>{title}</h4>
 			<div />
 		</div>
 	);
