e37b73c86019f550a43e66571255cb3b25ec7e75
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-05-07 12:40:30 +0700

    Adding the Services component to the home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,18 +2,22 @@ import React from 'react';
 import Hero from '../component/Hero';
 import Banner from '../component/Banner';
 import { Link } from 'react-router-dom';
+import Services from '../component/Services';
 
 export default function Home() {
 	return (
-		<Hero>
-			<Banner
-				title="luxurious rooms"
-				subtitle="deluxe rooms starting from $199"
-			>
-				<Link to="/rooms" className="btn-primary">
-					Our Rooms
-				</Link>
-			</Banner>
-		</Hero>
+		<>
+			<Hero>
+				<Banner
+					title="luxurious rooms"
+					subtitle="deluxe rooms starting from $199"
+				>
+					<Link to="/rooms" className="btn-primary">
+						Our Rooms
+					</Link>
+				</Banner>
+			</Hero>
+			<Services />
+		</>
 	);
 }
