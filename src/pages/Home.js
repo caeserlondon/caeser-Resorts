bbc5c828e776ff729c10f81a27704d0c2f2e139a
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-05-07 18:27:55 +0700

    Adding the featured rooms to the home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import Hero from '../component/Hero';
 import Banner from '../component/Banner';
 import { Link } from 'react-router-dom';
 import Services from '../component/Services';
+import FeaturedRooms from '../component/FeaturedRooms';
 
 export default function Home() {
 	return (
@@ -18,6 +19,7 @@ export default function Home() {
 				</Banner>
 			</Hero>
 			<Services />
+			<FeaturedRooms />
 		</>
 	);
 }
