2941ea5acb1949fc4a20a2dd1346613094a1bf8b
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-05-07 21:49:05 +0700

    Updating the Featured Room Component

--- src/component/FeaturedRooms.js
+++ src/component/FeaturedRooms.js
@@ -1,16 +1,28 @@
 // from -- API section -- https://reactjs.org/docs/context.html#reactcreatecontext  -- API --
 
 import React, { Component } from 'react';
-
 import { RoomContext } from '../context';
+import Loading from './Loading';
+import Room from './Room';
+import Title from './Title';
 
 export default class FeaturedRooms extends Component {
 	static contextType = RoomContext;
 
 	render() {
-		const { featuredRooms: rooms } = this.context;
+		let { loading, featuredRooms: rooms } = this.context;
 		// console.log(rooms);
+		rooms = rooms.map((room) => {
+			return <Room key={room.id} room={room} />;
+		});
 
-		return <div>Hello from FeaturedRooms</div>;
+		return (
+			<section className="featured-rooms">
+				<Title title="featured rooms" />
+				<div className="featured-rooms-center">
+					{loading ? <Loading /> : rooms}
+				</div>
+			</section>
+		);
 	}
 }
